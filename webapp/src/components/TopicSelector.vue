4763011857fc866aca819ad14ecd9cc6e85b84ba
david-i-berry <dberry@wmo.int>
2023-09-08 14:06:03 +0200

    Addition of csv form

---
+++ webapp/src/components/TopicSelector.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-card>
+    <v-card-item>
+      <v-select label="Channel" v-model="hierarchy" :items="hierarchyList" v-if="hierarchyList.length"
+        hint="Topic hierarchy for ingestion of data" persistent-hint></v-select>
+    </v-card-item>
+  </v-card>
+</template>
