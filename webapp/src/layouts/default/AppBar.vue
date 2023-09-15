30ce234baab71e0f48fbadb3e24f450921483ecf
RoryPTB <47696929+RoryPTB@users.noreply.github.com>
2023-09-15 16:29:53 +0200

    Removed buttons and improved navigation drawer

--- webapp/src/layouts/default/AppBar.vue
+++ webapp/src/layouts/default/AppBar.vue
@@ -2,31 +2,46 @@
   <v-app-bar image="@/assets/banner.png">
     <img src="@/assets/logo-large.png" alt="Logo" class="wmo-logo" />
 
+    <template v-slot:image>
+      <!-- Gradient from white to transparent, making the logo and text clearer on tablets/mobiles -->
+      <v-img gradient="to left, transparent, #FFFFFF"></v-img>
+    </template>
 
     <v-app-bar-title class="wis2-title"> WIS2 in a box
     </v-app-bar-title>
-    <v-btn class="bar-button" exact to="/">Home</v-btn>
-    <v-btn class="bar-button" text to="/synop_form">Synop form</v-btn>
-    <v-btn class="bar-button" text to="/csv2bufr_form">CSV to BUFR form</v-btn>
-    <v-btn class="bar-button" text to="/station">Stations</v-btn>
-    <v-btn class="bar-button" text to="/monitoring">Monitoring</v-btn>
+
+    <template v-slot:append>
+      <v-app-bar-nav-icon color="#F6FFF8" @click="drawer = !drawer"></v-app-bar-nav-icon>
+    </template>
+
   </v-app-bar>
 
-  <!-- Navigation drawer opens when hamburger menu clicked -->
-  <v-navigation-drawer v-if="isMobile" v-model="drawer" location="right">
-    <v-list dense nav>
-      <v-list-item exact to="/">
-        <v-list-item-title>HOME</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="synop_form">
-        <v-list-item-title>SYNOP FORM</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="csv2bufr_form">
-        <v-list-item-title>CSV FORM</v-list-item-title>
-      </v-list-item>
-      <v-list-item to="monitoring">
-        <v-list-item-title>MONITORING</v-list-item-title>
-      </v-list-item>
+  <v-navigation-drawer v-model="drawer" location="right" :width="200" :rail="rail" permanent>
+
+    <v-list-item :class="rail ? 'left-chevron' : 'right-chevron'">
+      <template v-slot:append>
+        <v-btn v-if="rail === true" variant="text" icon="mdi-chevron-left" @click.stop="rail = !rail"></v-btn>
+        <v-btn v-if="rail === false" variant="text" icon="mdi-chevron-right" @click.stop="rail = !rail"></v-btn>
+      </template>
+    </v-list-item>
+
+    <v-list v-model:opened="open" dense nav>
+      <v-list-item prepend-icon="mdi-home" title="Home" exact to="/" />
+
+      <v-list-group value="Forms">
+        <template v-slot:activator="{ props }">
+          <v-list-item v-bind="props" prepend-icon="mdi-notebook-edit" title="Forms" 
+          v-if="rail === true" @click.stop="rail = !rail" />
+          <v-list-item v-bind="props" prepend-icon="mdi-notebook-edit" title="Forms" 
+          v-else />
+        </template>
+
+        <v-list-item v-if="rail === false" title="SYNOP" to="synop_form" />
+        <v-list-item v-if="rail === false" title="CSV" to="csv2bufr_form" />
+      </v-list-group>
+
+      <v-list-item prepend-icon="mdi-satellite-uplink" title="Stations" to="station" />
+      <v-list-item prepend-icon="mdi-chart-box" title="Monitoring" to="monitoring" />
     </v-list>
   </v-navigation-drawer>
 </template>
@@ -36,15 +51,8 @@
 import { ref } from 'vue';
 
 const drawer = ref(false)
-
-// Initial state of boolean to determine if the page is viewed on mobile
-const isMobile = ref(window.innerWidth <= 960);
-
-// Event listener to update the boolean when the window size changes
-window.addEventListener('resize', () => {
-  isMobile.value = window.innerWidth <= 960;
-});
-
+const rail = ref(true)
+const open = ref(["Forms"])
 
 </script>
 
@@ -54,12 +62,21 @@ window.addEventListener('resize', () => {
 }
 
 .wmo-logo {
-  height: 70px;
+  height: 60px;
 }
 
 .wis2-title {
   color: #003DA5;
   font-size: 2rem;
   font-weight: 300;
+}
+
+.left-chevron {
+   justify-content: center;
+}
+
+.right-chevron {
+   justify-content: flex-start;
+   padding-left: 4px;
 }
 </style>
